3c079b7ceac0f48a01b08e4bb984bfe378ac56f3
AdebisiOluwanisolaDorcas <adebisioluwanisola1@gmail.com>
2026-06-29 18:53:28 -0400

    Fixed all messages

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -25,8 +25,7 @@ regd_users.post("/login", (req, res) => {
   if (authenticatedUser(username, password)) {
     let accessToken = jwt.sign({ username }, "access", { expiresIn: "1h" });
     req.session.authorization = { accessToken, username };
-    return res.status(200).json({ message: "User successfully logged in" });
-  } else {
+return res.status(200).json({ message: "Login successful!" });  } else {
     return res.status(401).json({ message: "Invalid credentials" });
   }
 });
@@ -52,8 +51,7 @@ regd_users.delete("/auth/review/:isbn", (req, res) => {
 
   if (books[isbn] && books[isbn].reviews[username]) {
     delete books[isbn].reviews[username];
-    return res.status(200).json({ message: `Review by ${username} deleted for ISBN ${isbn}` });
-  } else {
+return res.status(200).json({ message: `Review for ISBN ${isbn} deleted` });  } else {
     return res.status(404).json({ message: "Review not found" });
   }
 });
